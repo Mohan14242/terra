53ca3f11128512393fd9eb4021ef7f0f391928da
vijaymichiri <vijay165829@gmail.com>
2023-09-12 17:58:36 +0530

    this is terraform

--- provider.tf
+++ provider.tf
@@ -8,12 +8,12 @@ terraform {
   }
 }
 
-backend "s3"{
-  bucket="mohan14242"
-  key="mohankey"
-  region="us-east-1"
-  dynamodb_table="mohan14242"
-}
+# backend "s3"{
+#   bucket="mohan14242"
+#   key="mohankey"
+#   region="us-east-1"
+#   dynamodb_table="mohan14242"
+# }
 
 provider "aws" {
     region ="us-east-1"
